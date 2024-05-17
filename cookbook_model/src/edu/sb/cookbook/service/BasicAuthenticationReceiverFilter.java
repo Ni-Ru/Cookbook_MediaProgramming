a4ce8c3341a92ea8afbbff7c3ecc0e3a9b9dc118
pandanmochi <to.un.thi@gmail.com>
2024-05-17 16:03:02 +0200

    http authentication

--- cookbook_model/src/edu/sb/cookbook/service/BasicAuthenticationReceiverFilter.java
+++ cookbook_model/src/edu/sb/cookbook/service/BasicAuthenticationReceiverFilter.java
@@ -51,31 +51,18 @@ public class BasicAuthenticationReceiverFilter implements ContainerRequestFilter
 	 */
 	public void filter (final ContainerRequestContext requestContext) throws NullPointerException, ClientErrorException {
 		final MultivaluedMap<String,String> headers = requestContext.getHeaders();
-		// TODO:
-		// - Throw a ClientErrorException(Status.BAD_REQUEST) if the given context's headers map already contains a
-		//   "Requester-Identity" key, in order to prevent spoofing attacks.
+		
+		if (headers.containsKey(REQUESTER_IDENTITY)) throw new ClientErrorException(Response.Status.BAD_REQUEST);
+		
 		// - Remove the "Authorization" header from said map and store the first of it's values in a variable
 		//   "textCredentials", or null if the header value is either null or empty.
-		// - if the "textCredentials" variable is not null, parse it programmatically using Base64.getDecoder().decode(),
-		//   use the resulting byte array to create a new String instance, and store the resulting <email>:<password>
-		//   combination in variable "credentials". 
-		// - Perform the PQL-Query "select p from Person as p where p.email = :email"), using the credentials email
-		//   address part. Note that this query will go to the second level cache before hitting the database if the
-		//   Person#email field is annotated using @CacheIndex(updateable = true)! 
-		// - if the resulting people list contains exactly one element, calculate the hex-string representation
-		//   (i.e. 2 digits per byte) of the SHA2-256 hash code of the credential's password part using
-		//   HashCodes.sha2HashText(256, text).
-		// - if this hash representation is equal to queried person's password hash, add a new "Requester-Identity"
-		//   header to the request headers, using the person's identity (converted to String) as value, and return
-		//   from this method.
-		// - in all other cases, abort the request using requestContext.abortWith() in order to challenging the client
-		//   to provide HTTP Basic credentials (i.e. status code 401, and "WWW-Authenticate" header value "Basic").
-		//   Note that the alternative of throwing NotAuthorizedException("Basic") comes with the disadvantage that
-		//   failed authentication attempts clutter the server log with stack traces.
-		if (headers.containsKey(REQUESTER_IDENTITY)) throw new ClientErrorException(Response.Status.BAD_REQUEST);
 		final List<String> headerValues = headers.remove(HttpHeaders.AUTHORIZATION);
 		if (headerValues != null && !headerValues.isEmpty()) {
 			final String textCredentials = headerValues.get(0);
+
+			// - if the "textCredentials" variable is not null, parse it programmatically using Base64.getDecoder().decode(),
+			//   use the resulting byte array to create a new String instance, and store the resulting <email>:<password>
+			//   combination in variable "credentials". 
 			final String encodedCredentials = textCredentials.substring("Basic ".length());
 			final byte[] bytes = Base64.getDecoder().decode(encodedCredentials);
 			final String decodedCredentials = new String(bytes, StandardCharsets.ISO_8859_1);
@@ -83,15 +70,24 @@ public class BasicAuthenticationReceiverFilter implements ContainerRequestFilter
 			final String email = decodedCredentials.substring(0, indexOfFirstColon);
 			final String password = decodedCredentials.substring(indexOfFirstColon + 1);
 
-			final EntityManager entityManager = RestJpaLifecycleProvider.entityManager("secondhand");
+			// - Perform the PQL-Query "select p from Person as p where p.email = :email"), using the credentials email
+			//   address part. Note that this query will go to the second level cache before hitting the database if the
+			//   Person#email field is annotated using @CacheIndex(updateable = true)! 
+			final EntityManager entityManager = RestJpaLifecycleProvider.entityManager("local_database");
 			final List<Person> people = entityManager
 					.createQuery("SELECT p FROM Person AS p WHERE p.email = :email", Person.class)
 					.setParameter("email", email)
 					.getResultList();
 
+			// - if the resulting people list contains exactly one element, calculate the hex-string representation
+			//   (i.e. 2 digits per byte) of the SHA2-256 hash code of the credential's password part using
+			//   HashCodes.sha2HashText(256, text).
 			if (people.size() == 1) {
 				final String passwordHash = HashCodes.sha2HashText(256, password);
 				final Person requester = people.get(0);
+				// - if this hash representation is equal to queried person's password hash, add a new "Requester-Identity"
+				//   header to the request headers, using the person's identity (converted to String) as value, and return
+				//   from this method.
 				if (passwordHash.equals(requester.getPasswordHash())) {
 					headers.add(REQUESTER_IDENTITY, Long.toString(requester.getIdentity()));
 					return;
@@ -99,6 +95,10 @@ public class BasicAuthenticationReceiverFilter implements ContainerRequestFilter
 			}
 		}
 
+		// - in all other cases, abort the request using requestContext.abortWith() in order to challenging the client
+		//   to provide HTTP Basic credentials (i.e. status code 401, and "WWW-Authenticate" header value "Basic").
+		//   Note that the alternative of throwing NotAuthorizedException("Basic") comes with the disadvantage that
+		//   failed authentication attempts clutter the server log with stack traces.
 		final Response response = Response.status(Response.Status.UNAUTHORIZED).header(HttpHeaders.WWW_AUTHENTICATE, "Basic").build();
 		requestContext.abortWith(response);
 	}
